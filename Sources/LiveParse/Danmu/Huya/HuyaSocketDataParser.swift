1a11d1fbe377dd2c7d1aecefc8dc886ec5db78d3
pc <pangchongcyc@gmail.com>
2024-05-13 14:34:30 +0800

    Update HuyaSocketDataParser.swift
    修复虎牙卡顿问题

--- Sources/LiveParse/Danmu/Huya/HuyaSocketDataParser.swift
+++ Sources/LiveParse/Danmu/Huya/HuyaSocketDataParser.swift
@@ -10,17 +10,24 @@ import JavaScriptCore
 
 public class HuyaSocketDataParser: WebSocketDataParser {
     
+    let huyaJSContext: JSContext = {
+        let huyaJSContext = JSContext()
+        if let huyaFilePath = Bundle.module.path(forResource: "huya", ofType: "js") {
+            huyaJSContext?.evaluateScript(try? String(contentsOfFile: huyaFilePath))
+        }
+        return huyaJSContext!
+    }()
+    
     func performHandshake(connection: WebSocketConnection) {
         if let huyaFilePath = Bundle.module.path(forResource: "huya", ofType: "js") {
-            let huyaJSContext = JSContext()
-            huyaJSContext?.evaluateScript(try? String(contentsOfFile: huyaFilePath))
-            huyaJSContext?.evaluateScript("""
+            huyaJSContext.evaluateScript(try? String(contentsOfFile: huyaFilePath))
+            huyaJSContext.evaluateScript("""
                             var wsUserInfo = new HUYA.WSUserInfo;
                             wsUserInfo.lUid = "\(connection.parameters?["lYyid"] ?? "")";
                             wsUserInfo.lTid = "\(connection.parameters?["lChannelId"] ?? "")";
                             wsUserInfo.lSid = "\(connection.parameters?["lSubChannelId"] ?? "")";
                             """)
-            let result = huyaJSContext?.evaluateScript("""
+            let result = huyaJSContext.evaluateScript("""
                 new Uint8Array(sendRegister(wsUserInfo));
             """)
             let data = Data(result?.toArray() as? [UInt8] ?? [])
@@ -37,27 +44,23 @@ public class HuyaSocketDataParser: WebSocketDataParser {
     }
     
     private func deCodeData(data: Data, connection: WebSocketConnection) {
-        if let huyaFilePath = Bundle.module.path(forResource: "huya", ofType: "js") {
-            let huyaJSContext = JSContext()
-            huyaJSContext?.evaluateScript(try? String(contentsOfFile: huyaFilePath))
-            let bytes = [UInt8](data)
-            if let re = huyaJSContext?.evaluateScript("test(\(bytes));"), let json = try? JSONSerialization.jsonObject(with: Data((re.toString() ?? "").utf8), options: []) as? [String: Any] {
-                guard let str = json["sContent"] as? String else {
-                    return
-                }
-                let tFormat = json["tFormat"] as? [String: Any]
-                let col = tFormat?["iFontColor"] as? Int ?? -1
-                guard str != "HUYA.EWebSocketCommandType.EWSCmd_RegisterRsp" else {
-                    print("huya websocket inited EWSCmd_RegisterRsp")
-                    return
-                }
-                guard str != "HUYA.EWebSocketCommandType.Default" else {
-                    print("huya websocket WebSocketCommandType.Default \(data)")
-                    return
-                }
-                guard !str.contains("分享了直播间，房间号"), !str.contains("录制并分享了小视频"), !str.contains("进入直播间"), !str.contains("刚刚在打赏君活动中") else { return }
-                connection.delegate?.webSocketDidReceiveMessage(text: str, color: UInt32(getHuyaLiveColor(col: col)))
+        let bytes = [UInt8](data)
+        if let re = huyaJSContext.evaluateScript("test(\(bytes));"), let json = try? JSONSerialization.jsonObject(with: Data((re.toString() ?? "").utf8), options: []) as? [String: Any] {
+            guard let str = json["sContent"] as? String else {
+                return
             }
+            let tFormat = json["tFormat"] as? [String: Any]
+            let col = tFormat?["iFontColor"] as? Int ?? -1
+            guard str != "HUYA.EWebSocketCommandType.EWSCmd_RegisterRsp" else {
+                print("huya websocket inited EWSCmd_RegisterRsp")
+                return
+            }
+            guard str != "HUYA.EWebSocketCommandType.Default" else {
+                print("huya websocket WebSocketCommandType.Default \(data)")
+                return
+            }
+            guard !str.contains("分享了直播间，房间号"), !str.contains("录制并分享了小视频"), !str.contains("进入直播间"), !str.contains("刚刚在打赏君活动中") else { return }
+            connection.delegate?.webSocketDidReceiveMessage(text: str, color: UInt32(getHuyaLiveColor(col: col)))
         }
     }
     
